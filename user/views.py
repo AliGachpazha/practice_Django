0cb8ee393978afc036f0e22017443af478655331
ali <gachpazha2001@gmail.com>
2023-03-24 16:13:09 +0330

    write register form

--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from .forms import UserForms
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def register(request):
+    form = UserForms()
+    if request.method == 'POST':
+        form = UserForms(request.POST)
+        if form.is_valid():
+            form.save()
+            # username = form.cleaned_data.get('username')
+            # raw_password = form.cleaned_data.get('password')
+            # user = authenticate(username=username, password=raw_password)
+            # login(request, user)
+            return True
+
+    return render(request, 'user/html/register.html', {'form': form})
+
+
+def login():
+    pass
